2c07015ad1d3d7636959fed88683c879807f8aca
anant <anant.panchal@upayments.com>
2025-10-06 13:34:45 +0530

    change in the variable description

--- terraform-labs/aws-ec2-vpc-examples/variable.tf
+++ terraform-labs/aws-ec2-vpc-examples/variable.tf
@@ -1,5 +1,5 @@
 variable "aws_region_name" {
-  description = "defining the name of the instance"
+  description = "defining the name of the region"
   type        = string
   default     = "us-east-1"
 }
