b61093b6a4193288abaf2ea05cfd0f01961abf65
anant <anant.panchal@upayments.com>
2025-10-06 15:53:54 +0530

    s3 terraform example

---
+++ terraform-labs/aws_s3_example/main.tf
@@ -0,0 +1,15 @@
+#Defining the provider
+terraform {
+  required_providers {
+    aws ={
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+#creating the s3 bucket
